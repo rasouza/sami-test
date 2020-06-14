30def1b2e9f5fdbc4a8fb89356b820bebcbe2281
R. A. Souza <alves.wm@gmail.com>
2020-06-13 21:44:02 -0300

    creates mongoose schema

---
+++ backend/infrastructure/database/schemas/User.js
@@ -0,0 +1,15 @@
+'use strict'
+
+exports = module.exports = ({database: mongoose}) => mongoose.model('User', new mongoose.Schema({
+  name: String,
+  cpf: String,
+  birthdate: Date,
+  subscription: {
+    type: String,
+    enum: ['Basic', 'Standard', 'Premium']
+  },
+  dependents: Number
+}))
+
+exports['@singleton'] = true
+exports['@require'] = ['container']
