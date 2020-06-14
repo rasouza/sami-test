49773dd048bd6838c9b55e25c15214410c8bfe6c
R. A. Souza <alves.wm@gmail.com>
2020-06-14 20:04:07 -0300

    prepares bootstrapper for tests

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,18 @@
 require('dotenv').config()
 
 const IoC = require('electrolyte')
-const bootstrap = require('./infrastructure/config/bootstrap')
 
 IoC.use(IoC.node_modules())
 IoC.use('schemas', IoC.dir('infrastructure/database/schemas'))
 IoC.use(IoC.dir('infrastructure/config'))
 IoC.use(IoC.dir('.'))
 
-bootstrap.init()
-bootstrap.registerProviders()
-bootstrap.createServer()
+const app = async () => {
+  const bootstrap = await IoC.create('bootstrap')
+  bootstrap.init()
+  bootstrap.registerProviders()
+  
+  return bootstrap.createServer()
+}
+
+module.exports = app()
