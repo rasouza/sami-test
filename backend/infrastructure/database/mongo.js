49475f29baf8a1ef77fc5fc76b816afa2d38b1fe
R. A. Souza <alves.wm@gmail.com>
2020-06-13 21:42:28 -0300

    fixes mongodb connection

--- backend/infrastructure/database/mongo.js
+++ backend/infrastructure/database/mongo.js
@@ -1,12 +1,14 @@
 'use strict'
 
 exports = module.exports = mongoose => {
-  const { DB_HOST, DB_PORT, DB_DATABASE } = process.env
+  const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASS } = process.env
   const connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
 
   mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    user: DB_USER,
+    pass: DB_PASS
   });
   
   const db = mongoose.connection;
